8a79d208035d6208bf1c4c78f25a4a273404c713
Maro de Melo <maromelo@tempus.digital>
2021-11-15 14:46:58 -0300

    backend data api

--- app.js
+++ app.js
@@ -8,8 +8,68 @@ var db = mongojs('goldtracker', ['goals']);
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client')))
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.send('It works!');
+});
+
+app.get('/goals', function (req, res) {
+    db.goals.find(function (err, docs) {
+        if (err) {
+            res.send(err);
+        } else {
+            console.log('Getting goals...');
+            res.json(docs);
+        }
+    });
+});
+
+app.post('/goals', function (req, res) {
+    db.goals.insert(req.body, function (err, docs) {
+        if (err) {
+            res.send(err);
+        } else {
+            console.log(req.body);
+            console.log('Adding goals...');
+
+            res.json(docs);
+        }
+    });
+});
+
+app.put('/goals/:id', function (req, res) {
+    db.goals.findAndModify({
+        query: { _id: mongojs.ObjectID(req.params.id) },
+        update: {
+            $set: {
+                name: req.body.name,
+                name: req.body.type,
+                deadline: req.body.deadline,
+            }
+        },
+        new: true,
+    },
+
+        function (err, doc) {
+            if (err) {
+                res.send(err);
+            } else {               
+                console.log('Updating goal...');
+                res.json(doc);
+            }
+        });
+});
+
+app.delete('/goals/:id', function (req, res) {
+    db.goals.remove({ _id: mongojs.ObjectID(req.params.id) }, function (err, docs) {
+        if (err) {
+            res.send(err);
+        } else {
+            console.log(req.body);
+            console.log('Removing goals...');
+
+            res.json(docs);
+        }
+    });
 });
 
 app.listen(3000);
